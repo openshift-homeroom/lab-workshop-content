0b0c31ff54291bef2d31f1cb46ab06379be77505
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-07-29 22:24:16 +1000

    Add workshop settings.

---
+++ .workshop/settings.sh
@@ -0,0 +1,6 @@
+WORKSHOP_NAME=lab-workshop-content
+WORKSHOP_IMAGE=quay.io/openshifthomeroom/lab-workshop-content:1.5
+SPAWNER_MODE=learning-portal
+RESOURCE_BUDGET=small
+MAX_SESSION_AGE=3600
+IDLE_TIMEOUT=300
